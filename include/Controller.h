9f780056b3b32decb7610f02cabb2ffa0660dbb4
Andrew Keats <steakeye.studios@gmail.com>
2019-03-26 21:12:03 +0000

    wip: started work on the controller

--- include/Controller.h
+++ include/Controller.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <interface/IPlayer.h>
+#include <interface/IRenderer.h>
+#include <Board.h>
+
+class Controller {
+public:
+    Controller(IRenderer renderer, Board board, IPlayer playerOne, IPlayer playerTwo);
+
+    void PlayGame();
+
+private:
+    IPlayer& playerOne;
+    IPlayer& playerTwo;
+    Board board;
+    IRenderer& renderer;
+};
--- include/HumanPlayer.h
+++ include/HumanPlayer.h
@@ -3,11 +3,12 @@
 #include <interface/IPlayer.h>
 
 class HumanPlayer : public IPlayer {
-    char character;
-
 public:
     HumanPlayer(char character);
 
     char Symbol() const override;
     BoardPosition TakeTurn(const std::vector<BoardPosition>& freePositions) override;
+
+private:
+    char character;
 };
