35931a3807ad05856608303014d22a74208ee2e8
Andrew Keats <steakeye.studios@gmail.com>
2019-05-29 00:17:13 +0100

    wip: trying to get the project to build

--- src/HumanPlayer.cpp
+++ src/HumanPlayer.cpp
@@ -7,13 +7,13 @@ char HumanPlayer::Symbol() const { return character; }
 
 BoardPosition HumanPlayer::TakeTurn(const std::vector<BoardPosition>& freePositions) {
     PrintMessage("Player ", character, ", take your turn from:");
-    PrintMessage(freePositions);
+    PrintMessage(&freePositions);
 
     int userInput = ReadNumber(0, 8);
 
     BoardPosition userPosition = NumberToBoardPosition(userInput);
 
-    std::vector<BoardPosition&>::iterator endIterator = freePositions.end();
+    std::vector<BoardPosition>::const_iterator endIterator = freePositions.end();
     std::vector<BoardPosition>::const_iterator positionIndex = GetFreePositionIt(freePositions, userPosition);
 
     bool positionIsFree = positionIndex != endIterator;
