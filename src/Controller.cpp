742fb4651b4e99835882b2bcbaf3513619db52cf
Andrew Keats <steakeye.studios@gmail.com>
2019-06-04 19:53:56 +0100

    Proof that the render works
    the board will be displayed at the start

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -3,6 +3,7 @@
 Controller::Controller(IRenderer& renderer, Board board, IPlayer& playerOne, IPlayer& playerTwo): renderer(renderer), board(board), playerOne(playerOne), playerTwo(playerTwo) {}
 
 void Controller::PlayGame() {
-    PrintMessage("Welcome to Noughts and Crosses!");
+    PrintMessage("Welcome to Noughts and Crosses!\n");
     // TODO!
+    renderer.RenderBoard(board.CurrentState());
 }
